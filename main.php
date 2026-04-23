9b43812f69f452c3dc6901b72734104e8d54812f
vinicius <vinicius.valim@edu.senai.br>
2026-04-23 11:42:16 -0300

    v0.3 - criada a funcao de subtrair o projeto

--- main.php
+++ main.php
@@ -1,8 +1,12 @@
 <?php
 
 
-echo "na aula do dia 23.04.2026 eu aprendi a mexer no github/git e desenvolver funções no php!";
+function somar($a, $b){
+ return $a + $b;
+} 
 
-
+function subtrair($a, $b){
+    return $a - $b;
+}
 
 ?>
