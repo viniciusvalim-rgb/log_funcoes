6768163f22dcd9a049a7ceb71cdfd7e4ef0c6bcc
vinicius <vinicius.valim@edu.senai.br>
2026-04-28 10:51:19 -0300

    acrescentando geometria

--- biblioteca.php
+++ biblioteca.php
@@ -16,12 +16,12 @@ namespace matematica{
     function dividir($a,$b){
         return $a /$b;
     }
-}
+    }
 namespace texto{
     function concatenar($nome, $sobrenome){
         return $nome . " ". $sobrenome;
     }
-}
+    }
 namespace logica{
     function idade($id){
         if ($id >= 18){
@@ -40,5 +40,41 @@ namespace logica{
             return "voce nao cumpre os requisitos";
         }
     }
-}
+    }
+namespace conversao{
+    function dolar($cotadolar, $real) {
+        return $cotadolar * $real;
+    }
+    function euro($cotaeuro, $real){
+        return $cotaeuro * $real;
+    }
+    function peso($cotapeso, $real){
+        return $cotapeso * $real;
+    }
+    function libra($cotalibra, $real){
+        return $cotalibra * $real;
+    }
+    function eine($cotaeine, $real){
+        return $cotaeine * $real;
+    }
+    }
+namespace geometria{
+    function areaquadrado($lado){
+        return $lado * $lado;
+    }
+    function arearetangulo($base, $altura){
+        return $base * $altura;
+    }
+    function areatriangulo($base, $altura){
+        return ($base * $altura) / 2;
+    }
+    function areacirculo($raio){
+      return 3.14 * ($raio * $raio);
+    }
+    function areatrapezio($basemaior, $basemenor, $altura){
+       return (($basemaior + $basemenor) * $altura) / 2;
+    }
+    }
+
+
 ?>
--- entrada.php
+++ entrada.php
@@ -3,10 +3,45 @@
 require_once "biblioteca.php";
 
 use function matematica\somar;
+use function matematica\subtrair;
+use function matematica\multiplicar;
+use function matematica\dividir;
+
 use function texto\concatenar;
+
 use function logica\exercito;
-echo "soma: ", somar (2,3);
+
+use function conversao\dolar;
+use function conversao\euro;
+use function conversao\peso;
+use function conversao\libra;
+use function conversao\eine;
+
+use function geometria\areaquadrado;
+use function geometria\arearetangulo;
+use function geometria\areatriangulo;
+use function geometria\areacirculo;
+use function geometria\areatrapezio;
+
+echo "valor em dolar: ", dolar (5,3);
+echo "\nvalor em euro: ", euro (5.85,3);
+echo "\nvalor em peso: ", peso (0.0035,3);
+echo "\nvalor em libra: ", libra (6.74,3);
+echo "\nvalor em eine: ", eine (0.031,3);
+
+echo "\narea do quadrado: ", areaquadrado(8);
+echo "\narea do retangulo: ", arearetangulo(4, 4);
+echo "\narea do triangulo: ", areatriangulo(4, 4);
+echo "\narea do circulo: ", areacirculo(4);
+echo "\narea do trapezio: ", areatrapezio(4, 6, 4);
+
+echo "\nsoma: ", somar (2,3);
+echo "\nsubtracao: ", subtrair (8, 4);
+echo "\nmultiplicacao: ", multiplicar(3, 3);
+echo "\ndivisao: ", dividir(9, 3);
+
 echo "\nnome: ", concatenar("vinicius","borges");
+
 echo "\nexercito: ", exercito(18, "masculino");
 
 
